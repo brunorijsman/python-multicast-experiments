57a08336a4327a74138ec31d5b7633d476297925
Bruno Rijsman <brunorijsman@gmail.com>
2019-01-04 16:13:42 -0600

    Docker environment for running test

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM ubuntu:latest
+RUN apt-get update
+RUN apt-get install -y python3
+RUN apt-get install -y python3-pip
+RUN apt-get install -y iproute2
+RUN apt-get install -y iputils-ping
+RUN apt-get install -y telnet
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+VOLUME /host
---
+++ setup-netns.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Setup the following network namespaces (netns) and virtual ethernet (veth) interface pairs:
+#
+#    ...............    ............................    ...............
+#    . netns-1     .    . netns-2                  .    . netns-3     .
+#    .             .    .                          .    .             .
+#    . veth-1-1    .    . veth-2-1        veth-2-2 .    . veth-3-1    .
+#    .    X------------------X             X-----------------X        .
+#    . 99.1.2.1/24 .    . 99.1.2.2/24  99.2.3.2/24 .    . 99.2.3.3/24 .
+#    ...............    ............................    ...............
+
+ip link add dev veth-1-1 type veth peer name veth-2-1
+ip link add dev veth-2-2 type veth peer name veth-3-1
+
+ip netns add netns-1
+ip link set veth-1-1 netns netns-1
+ip netns exec netns-1 ip link set dev veth-1-1 up
+ip netns exec netns-1 ip addr add 99.1.2.1/24 dev veth-1-1
+
+ip netns add netns-2
+ip link set veth-2-1 netns netns-2
+ip link set veth-2-2 netns netns-2
+ip netns exec netns-2 ip link set dev veth-2-1 up
+ip netns exec netns-2 ip addr add 99.1.2.2/24 dev veth-2-1
+ip netns exec netns-2 ip link set dev veth-2-2 up
+ip netns exec netns-2 ip addr add 99.2.3.2/24 dev veth-2-2
+
+ip netns add netns-3
+ip link set veth-3-1 netns netns-3
+ip netns exec netns-3 ip link set dev veth-3-1 up
+ip netns exec netns-3 ip addr add 99.2.3.3/24 dev veth-3-1
